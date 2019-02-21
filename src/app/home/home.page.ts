b45856662da7e1093f4548af61d3ac4296b81724
shrihari1999 <shrihari1999@gmail.com>
2019-02-22 00:22:07 +0530

    Demo login page
    Login page has been designed for demo purposes, basic version.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(public alertController: AlertController, private storage: Storage){
+    this.checkFirstTime();
+  }
+
+  async popAlert()
+  {
+    const alert = await this.alertController.create({
+      header:'Hey!',
+      subHeader:'Looks like its your first time here',
+      message:'Login with you PWI credentials',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  checkFirstTime()
+  {
+    this.storage.get('first_time').then((val) => {
+      if (val == null) {
+         this.storage.set('first_time', 'false');
+         this.popAlert();
+      }
+   });
+  }
+
+  login()
+  {
+    //call auth service
+  }
 
 }
