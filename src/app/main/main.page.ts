54ee0cc84ef8ab6e1245a2375ce91f9153d13f51
shrihari1999 <shrihari1999@gmail.com>
2019-03-21 03:29:50 +0530

    Added structure for multiple menu items

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { AlertController } from '@ionic/angular';
 
@@ -7,8 +7,7 @@ import { AlertController } from '@ionic/angular';
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
 })
-export class MainPage implements OnInit {
-
+export class MainPage implements OnInit{
   regnum="";
   pswrd="";
   public menu=[
@@ -16,26 +15,98 @@ export class MainPage implements OnInit {
     {tag:'itemtag2', icon:'sunny', val:'Noodles', note:'Add:', isChecked:false, color:'primary', price:40},
     {tag:'itemtag3', icon:'moon', val:'Paneer Fried Rice', note:'Added!', isChecked:true, color:'danger', price:50}
   ];
+  public oldmenu=[
+    {tag:'itemtag', icon:'partly-sunny', val:'Old Masala Dosa', hasOrdered:true, code:'ksjdgh', color:'success'},
+    {tag:'itemtag2', icon:'sunny', val:'Old Noodles', hasOrdered:false, code:'sjffdd', color:'primary'},
+    {tag:'itemtag3', icon:'moon', val:'Old Masala Dosa', hasOrdered:true, code:'sdjfsd', color:'danger'}
+  ];
+  disablekey=false;
 
   constructor(private storage: Storage, public alertController: AlertController) { }
 
-  updateNote(){
-      var d = new Date();
-      if(d.getHours() > 7 && d.getHours() < 23 ){
+  updateCode(){
+    var num=null,date=null,d=new Date();
+    num=1;//find and store size of meal count
+    date=19;//get and store today's date
+    this.oldmenu.splice(0,this.oldmenu.length);
+    for (let i = 0; i < num; i++)
+    {
+    this.oldmenu.push({tag:'itemtag', icon:'partly-sunny', val:'', hasOrdered:false, code:'', color:'success'});
+    }
+    //detect no. of lunch and store it in num
+    for (let i = 0; i < num; i++)
+    {
+    this.oldmenu.push({tag:'itemtag2', icon:'sunny', val:'', hasOrdered:false, code:'', color:'primary'});
+    }
+    //detect no. of dinner and store it in num
+    for (let i = 0; i < num; i++)
+    {
+    this.oldmenu.push({tag:'itemtag3', icon:'moon', val:'', hasOrdered:false, code:'', color:'danger'});
+    }
+    //updation starts
+    this.oldmenu.forEach(item => {
+      item.val='New Val!';//get new val from db
+      item.code='abcdef';//check if code exists in db and store here
+      item.hasOrdered=(item.code!='');
+    });
+  }
+
+  updateMenu()
+  { var num=null,date=null,d=new Date();
+    num=2;//find and store size of meal count
+    date=20//get and store tomorrow's date
+    this.menu.splice(0,this.menu.length);
+    //detect no. of brkfast and store it in num
+    for (let i = 0; i < num; i++)
+    {
+    this.menu.push({tag:'itemtag', icon:'partly-sunny', val:'', note:'', isChecked:false, color:'success', price:null});
+    }
+    //detect no. of lunch and store it in num
+    for (let i = 0; i < num; i++)
+    {
+    this.menu.push({tag:'itemtag2', icon:'sunny', val:'', note:'', isChecked:false, color:'primary', price:null});
+    }
+    //detect no. of dinner and store it in num
+    for (let i = 0; i < num; i++)
+    {
+    this.menu.push({tag:'itemtag3', icon:'moon', val:'', note:'', isChecked:false, color:'danger', price:null});
+    }
+    //updation starts
+    this.menu.forEach(item => {
+      item.val='New Val!';//get new val from db
+      item.note='Add:';
+      item.isChecked=false;
+      item.price=50;//get new price from db
+    });
+  }
+
+  updateOrder(){
+      this.checkTimeUp();
+      if(!this.disablekey){
           this.menu.forEach(item => {
-            if(item.isChecked){
-              item.note='Added!';
+            if(item.isChecked){ //ORDER MUST BE UPDATED
+              item.note='Added!';  //IF SUCCESSFUL, NOTE SHOULD BE CHANGED
             }
             else{
-              item.note='Add:';
+              item.note='Add:';//DISPLAY ERROR ALERT IF NOT SUCCESSFUL
             }
           });
       }else{
-          this.timeOut();
+          this.showTimeout();
       }
   }
 
-  async timeOut(){
+  checkTimeUp(){
+    var d = new Date();
+      if(d.getHours() > 7 && d.getHours() < 23 ){
+        this.disablekey=false;
+      }
+      else{
+          this.disablekey=true;
+      }
+  }
+
+  async showTimeout(){
       const alert = await this.alertController.create({
         header:'Sorry',
         subHeader:'Orders are now Closed',
@@ -46,12 +117,18 @@ export class MainPage implements OnInit {
       await alert.present();
   }
 
-  async showCode(){
+  async showCode(val){
+    var code = '';
+    this.oldmenu.forEach(item => {
+      if(item.val==val){
+        code=item.code;
+      }
+    });
     //show verification code
     const alert = await this.alertController.create({
       header:'Verification Code',
       subHeader:'Show this code to get your meal!',
-      message: this.getCode(),
+      message: code,
       buttons: ['OK']
     });
 
@@ -65,5 +142,9 @@ export class MainPage implements OnInit {
   ngOnInit() {
     this.storage.get('reg_num').then(val =>{this.regnum=val});
     this.storage.get('pswrd').then(val =>{this.pswrd=val});
+    this.checkTimeUp();
+    //call func - read from db, modify list size, update list values
+    this.updateCode();
+    this.updateMenu();
   }
 }
